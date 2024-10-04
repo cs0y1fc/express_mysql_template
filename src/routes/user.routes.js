2032a3f06bd63acd5a3a680139e451bb431c73d3
cs0y1fc <fredmcgregor0@gmail.com>
2024-10-04 23:56:02 +0200

    hola3

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -6,7 +6,7 @@ const userController = require("../controllers/user.controller.js");
 router.get("/users", userController.getAllUsers);
 router.get("/user/:id", userController.getUserById);
 router.post("/user", userController.createUser);
-// router.put("/user/:id", userController.updateUser);
-// router.delete("/user/:id", userController.deleteUser);
+router.put("/user/:id", userController.updateUser);
+router.delete("/user/:id", userController.deleteUser);
 
 module.exports = router;
